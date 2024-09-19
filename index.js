3c8da2f426499ac09f1ee66a4cfe7b2be74823fe
LamourMarine <marine.lamour@gmx.fr>
2024-09-19 11:00:11 +0200

    MobileFirst

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const btnToggle = document.querySelector(".btnToggle");
 
 btnToggle.addEventListener("click", () => {
-  const card = document.querySelectorAll(".card");
+  const cards = document.querySelectorAll(".card");
   const badges = document.querySelectorAll(".badge");
 
-  card.forEach((card) => {
-    if (card.classList.contains("dark")) {
-      card.classList.remove("dark");
-      card.classList.add("light");
-      btnToggle.innerHTML = "Go dark";
-    } else {
-      card.classList.add("dark");
-      card.classList.remove("light");
-      btnToggle.innerHTML = "Go light";
-    }
+  cards.forEach((card) => {
+    card.classList.toggle("dark");
+    card.classList.toggle("light");
   });
 
   badges.forEach((badge) => {
-    if (badge.classList.contains("dark")) {
-      badge.classList.remove("dark");
-      badge.classList.add("light");
-    } else {
-      badge.classList.add("dark");
-      badge.classList.remove("light");
-    }
+    badge.classList.toggle("dark");
+    badge.classList.toggle("light");
   });
-});
 
+  if (btnToggle.innerHTML === "Go dark") {
+    btnToggle.innerHTML = "Go light";
+  } else {
+    btnToggle.innerHTML = "Go dark";
+  }
+});
